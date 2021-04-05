9e082e8dfd4f1e02eaaa057329b7804094ee7308
jayanthsps <76771253+jayanthsps@users.noreply.github.com>
2021-04-05 14:03:04 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,13 @@
+class Form {
+    constructor(){
+
+    }
+    display(){
+        var title=createElement("h2")
+        title.html(" car racing game ");
+        title.position(130,0);
+        var input=createInput("name")
+        var button=createButton("play")
+        var greeting=createElement("h3")
+    }
+}
